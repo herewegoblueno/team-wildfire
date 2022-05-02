32d158bf52e63f9daa2fa3fcf5daf28349ad97c3
Anderson <34724755+andersonaddo@users.noreply.github.com>
2022-05-02 18:05:29 -0400

    Update physics.cpp

--- simulation/physics.cpp
+++ simulation/physics.cpp
@@ -6,14 +6,6 @@ double ambientTemperatureFunc(glm::dvec3 point){
     return glm::clamp(3.5 - point.y / 2, 0.0, 3.5 );
 }
 
-double maxReasonableCelcuis = 200;
-double minReasonableCelcuis = 20;
-double celciusdiff = maxReasonableCelcuis - minReasonableCelcuis;
-
-double maxSimulationTemp = 20;
-double minSimulationTemp = 2;
-double simDiff = maxSimulationTemp - minSimulationTemp;
-
 double simTempToWorldTemp(double simTemp){
     return minReasonableCelcuis + ((simTemp - minSimulationTemp) / simDiff) * celciusdiff;
 }
